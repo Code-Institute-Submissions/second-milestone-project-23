fd3d563158cd8c8af2acf8d822a03ae960f4c640
jesshperez <jessicaperez2911@gmail.com>
2020-12-08 14:07:16 +0000

    added map feature to index page +  making it suitable for mobile view, ipad view and desktop

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+function initMap(){
+            var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 1.5,
+            center: {lat: 10.00, lng: 10.00}
+            });
+        }

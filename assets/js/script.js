cca37e83a70478dbb585741569d77d10bc2f8ef7
jesshperez <jessicaperez2911@gmail.com>
2020-12-10 23:00:12 +0000

    added markers to map

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,68 @@ toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
 
+//CREDIT: Function by Traversy Media's Google Maps JavaScript API Tutorial<a href="https://www.youtube.com/watch?v=Zxf1mnP5zcw"></a>
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 1.5,
+
+    var options = {
+        zoom: 2,
         center: {
-            lat: 10.00,
-            lng: 10.00
+            lat: 26.8206,
+            lng: 30.8025
+        },
+    }
+    var map = new google.maps.Map(document.getElementById('map'), options);
+
+    var markers = [{
+            coords: {
+                lat: 23.1136,
+                lng: -82.3666,
+            },
+            content: '<h3>Havana, Cuba</h3>'
+        },
+        {
+            coords: {
+                lat: 58.0092,
+                lng: 56.2270,
+            },
+            content: '<h3>Perm, Russia</h3>'
+        },
+        {
+            coords: {
+                lat: 16.8409,
+                lng: 96.1735
+            },
+            content: '<h3>Yangon, Myanmar</h3>'
+        },
+        {
+            coords: {
+                lat: 22.5726,
+                lng: 88.3639
+            },
+            content: '<h3>Kolkata, India</h3>'
         }
-    });
+    ]
+
+    for (var i = 0; i < markers.length; i++) {
+        addMarker(markers[i]);
+    }
+
+    function addMarker(props) {
+        var marker = new google.maps.Marker({
+            position: props.coords,
+            map: map
+        });
+        if (props.content) {
+            var infoWindow = new google.maps.InfoWindow({
+                content: props.content
+            });
+            marker.addListener('click', function() {
+                infoWindow.open(map, marker);
+            });
+        }
+    }
 }
+//--- END CREDIT ---
 
 fetch("../assets/data/activities.json").then((result) => {
     const currentPath = window.location.pathname;
